3854d97f4e50e494c4608d4c4cbb8ed95bef6715
Wilfrido Almache <walmache@gmail.com>
2024-06-22 20:39:27 -0500

    ajustes

--- unidad2/casoPractico/faceRecognition/casoPractico2_3.py
+++ unidad2/casoPractico/faceRecognition/casoPractico2_3.py
@@ -1,13 +1,18 @@
+#instalación de librerías necesarias 
+%pip install face-recognition
+%pip install opencv-contrib-python  #Adición de módulos avanzados para manejo de objetos y reconocimiento
+%pip install tabulate
+
 #Importar módulos necesarios
 import cv2  #Manejo de imagenes
 import os   #Manejo de archivos/directorios
-import face_recognition
-from datetime import datetime
-from tabulate import tabulate
+import face_recognition         # Linreria de reconocimiento facial 
+from datetime import datetime   # Para el registro de fecha y hora de asistencia
+from tabulate import tabulate   #Tabulación de resultados de aistencia
 
 import imghdr  #Para validar si un archivo es imagen
-import sys
-import matplotlib.pyplot as plt
+import sys     #Manejo de comandos de S.O. 
+import matplotlib.pyplot as plt   #Para gráficos
 
 #Constantes a usar
 RED = "\033[91m"
@@ -17,7 +22,6 @@ BLUE = "\033[94m"
 RESET = "\033[0m"
 
 #Definición de funciones necesarias
-
 #Evalua si un archivo es imagen válida
 def isImage(filePath):
     return imghdr.what(filePath) is not None
@@ -29,23 +33,24 @@ def existsDirectory(directoryPath):
 #Visualizasión de mensajes 
 def message(text, color):
     print(f"{color}{text}{RESET}")
-    
+
 #Registro de Asistencias
 def attendanceRegister(student):
-    for registro in attendance:
-        # Verificar si el estudiante ya está registrado
-        if registro['nombre'] == student:
-            message(f"{student} ya está registrado.",RED)
-            return
-    hora_registro = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    registro = {'nombre': student, 'hora': hora_registro}
-    attendance.append(registro)
-    
+    if student!='Desconocido':
+        for registro in attendance:
+            # Verificar si el estudiante ya está registrado
+            if registro['nombre'] == student:
+                ##message(f"{student} ya está registrado.",RED)
+                return
+        hora_registro = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        registro = {'nombre': student, 'hora': hora_registro}
+        attendance.append(registro)
 
 def printReport():
     tabla_asistencia = [[record['nombre'], record['hora']] for record in attendance]
     message("\nRegistros de asistencia:".center(100),BLUE)
     print(tabulate(tabla_asistencia, headers=["Nombre del Estudiante", "Hora de Registro"]))
+    
     
 # Reconocimiento y alamacenamiento de rostros de imagenes aleatorias
 
@@ -86,18 +91,21 @@ for imageName in os.listdir(sourceImages):
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             face = image[y:y + h, x:x + w]
             face = cv2.resize(face, (150, 150))
-            #cv2.imwrite(facesRepo + "/" + str(i) + ".jpg", face)
+            cv2.imwrite(facesRepo + "/" + str(i) + ".jpg", face)
             i += 1
-            cv2.imshow("Rostros", face)
-            #cv2.waitKey(0)
+            ###cv2.imshow("Rostros", face)
+            ###cv2.waitKey(0)
         cv2.imshow("Image", image)
-        #cv2.waitKey(0)
+        ###cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-#Guardar embeding nombres y registros asistencia
+    
+    
+    #Guardar embeding nombres y registros asistencia
 facesEncodings = []
 facesNames = []
 attendance = []
+
 
 #Bucle para obtener los codigos de codificacion facial de los rostros almacenados
 for face in os.listdir(facesRepo):
@@ -106,11 +114,14 @@ for face in os.listdir(facesRepo):
     f_coding = face_recognition.face_encodings(image, known_face_locations=[(0, 150, 150, 0)])[0]
     facesEncodings.append(f_coding)
     facesNames.append(face.split(".")[0])
-#print(facesEncodings)
-#print(facesNames)
+###message("Embeding de los rostros", GREEN)
+###print(facesEncodings)
+###message("Nombres de rostros a buscar", GREEN)
+###print(facesNames)
 
 #Lectura del video 
-stream = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+##stream = cv2.VideoCapture(0, cv2.CAP_DSHOW) backen para windows
+stream = cv2.VideoCapture(0,cv2.CAP_V4L2)  #backend para captura de video recomentdado para linux
 
 #Modelo para la detección Facial
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -149,8 +160,8 @@ while True:
             index = result.index(True)
             name = facesNames[index]
             color = (125, 220, 0)
-            message("Registrar asistencia para "+name, GREEN)
-            attendanceRegister(name)
+            ##message("Registrar asistencia para "+name, GREEN)
+            ###attendanceRegister(name)
         else:
             name = "Desconocido"
             color = (50, 50, 255)
@@ -158,6 +169,9 @@ while True:
         cv2.rectangle(frame, (x, y + h), (x + w, y + h + 30), color, -1)
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
         cv2.putText(frame, name, (x, y + h + 25), 2, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        ##message("Registrar asistencia para "+name, GREEN)
+        attendanceRegister(name)
+
     cv2.imshow("Frame", frame)
     
     k = cv2.waitKey(1) & 0xFF
@@ -165,4 +179,4 @@ while True:
         break
 stream.release()
 cv2.destroyAllWindows()
-printReport() 
+printReport()
