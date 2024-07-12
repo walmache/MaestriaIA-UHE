52ab28a3474cbabde6d8e04feddd101f2571d78b
Wilfrido Almache <walmache@gmail.com>
2024-07-11 19:55:46 -0500

    unoidad2

---
+++ 2. CienciaAnaliticaDatos/Unidad2/decision_tree_2.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: IVAN
+"""
+
+# Decision Tree for breast cancer
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+
+# Carga el conjunto de datos Breast Cancer
+dataset = load_breast_cancer()
+X = dataset.data # 569x30
+y = dataset.target # 569x1
+
+# Divide el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normaliza los datos para que todas las características tengan una escala similar
+scaler = MinMaxScaler(feature_range=(0,1)) # [0, 1]
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+# Crea y entrena el modelo de árbol de decisión
+model = DecisionTreeClassifier(max_depth=4, criterion = 'gini')
+model.fit(X_train, y_train)
+
+# Realiza predicciones usando el conjunto de prueba
+y_pred = model.predict(X_test)
+
+
+# Convierte las probabilidades en etiquetas binarias (0 o 1)
+y_pred = (y_pred > 0.5)
+
+# Muestra el informe de evaluación del modelo entrenado
+print(classification_report(y_test, y_pred))
+
+# Matriz de confusión:
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_test, y_pred)
+print("confusion matrix: \n", cm)
+# gráfica cm
+plt.figure(figsize = (8,4))
+sns.heatmap(cm, annot=True, fmt='d')
+plt.xlabel('Prediction', fontsize = 12)
+plt.ylabel('Real', fontsize = 12)
+plt.show()
+
+# Exactitud:
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, y_pred)
+print("accuracy: ", acc)
+
+# Sensibilidad:
+from sklearn.metrics import recall_score
+recall = recall_score(y_test, y_pred)
+print("recall: ", recall)
+
+# Precisión:
+from sklearn.metrics import precision_score
+precision = precision_score(y_test, y_pred)
+print("precision: ", precision)
+
+# Especificidad
+# 'specificity' is just a special case of 'recall'. 
+# specificity is the recall of the negative class
+specificity = recall_score(y_test, y_pred, pos_label=0)
+print("specificity: ", specificity)
+
+# Puntuación F1:
+from sklearn.metrics import f1_score
+f1 = f1_score(y_test, y_pred)
+print("f1 score: ", f1)
+
+# Área bajo la curva:
+from sklearn.metrics import roc_auc_score
+auc = roc_auc_score(y_test, y_pred)
+print("auc: ", auc)
+
+# Curva ROC
+from sklearn.metrics import roc_curve
+plt.figure()
+lw = 2
+plt.plot(roc_curve(y_test, y_pred)[0], roc_curve(y_test, y_pred)[1], color='darkorange',lw=lw, label='ROC curve (area = %0.2f)' %auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+# R Score (R^2 coefficient of determination)
+from sklearn.metrics import r2_score
+R = r2_score(y_test, y_pred)
+print("R2: ", R)
+
+
+# Visualizar un árbol de decisión usando matplotlib
+from sklearn.tree import plot_tree
+# Crear la figura y el eje
+fig, ax = plt.subplots(figsize=(24, 20))
+# Dibujar el árbol de decisión
+plot_tree(model, 
+                feature_names = dataset.feature_names, 
+                class_names = dataset.target_names, 
+                filled=True, 
+                rounded=True, 
+                ax=ax)
+# Mostrar la gráfica
+plt.show()
+
+# Calcular y visualizar la importancia de las variables en la predicción del modelo
+importances = model.feature_importances_
+
+# Crear un DataFrame para visualizar las importancias
+import pandas as pd
+feature_importances = pd.DataFrame({
+    'Variable': dataset.feature_names,
+    'Importancia': importances
+}).sort_values(by='Importancia', ascending=False)
+
+print(feature_importances)
+
+# Visualizar las importancias de las variables
+plt.figure(figsize=(12, 8))
+plt.barh(feature_importances['Variable'], feature_importances['Importancia'])
+plt.xlabel('Importancia')
+plt.ylabel('Variables')
+plt.title('Importancia de las variables')
+plt.gca().invert_yaxis()
+plt.show()
+
+# Guardar el modelo a un archivo
+import joblib
+joblib.dump(model, 'decision_tree_model.pkl')
+# Cargar el modelo desde el archivo
+loaded_model = joblib.load('decision_tree_model.pkl')
+# Hacer predicciones con el modelo cargado
+y_pred = model.predict(X_test)
---
+++ 2. CienciaAnaliticaDatos/Unidad2/logistic_regression_2.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 11:22:16 2023
+
+@author: IVAN
+"""
+
+# Logistic regression for breast cancer
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+
+# Carga el conjunto de datos Breast Cancer
+dataset = load_breast_cancer()
+X = dataset.data # 569x30
+y = dataset.target # 569x1
+
+# Divide el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normaliza los datos para que todas las características tengan una escala similar
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Crea y entrena el modelo de regresión logistica
+model = LogisticRegression(multi_class='auto', solver='lbfgs', max_iter=100)
+model.fit(X_train, y_train)
+
+# Imprime los coeficientes y el intercepto del modelo entrenado
+print("\nCoeficientes del modelo:")
+print(model.coef_)
+print("\nIntercepto del modelo:")
+print(model.intercept_)
+
+# Realiza predicciones usando el conjunto de prueba
+y_pred = model.predict(X_test)
+
+
+# Convierte las probabilidades en etiquetas binarias (0 o 1)
+y_pred = (y_pred > 0.5)
+
+# Muestra el informe de evaluación del modelo entrenado
+print(classification_report(y_test, y_pred))
+
+# Matriz de confusión:
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_test, y_pred)
+print("confusion matrix: \n", cm)
+# gráfica cm
+plt.figure(figsize = (8,4))
+sns.heatmap(cm, annot=True, fmt='d')
+plt.xlabel('Prediction', fontsize = 12)
+plt.ylabel('Real', fontsize = 12)
+plt.show()
+
+# Exactitud:
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, y_pred)
+print("accuracy: ", acc)
+
+# Sensibilidad:
+from sklearn.metrics import recall_score
+recall = recall_score(y_test, y_pred)
+print("recall: ", recall)
+
+# Precisión:
+from sklearn.metrics import precision_score
+precision = precision_score(y_test, y_pred)
+print("precision: ", precision)
+
+# Especificidad
+# 'specificity' is just a special case of 'recall'. 
+# specificity is the recall of the negative class
+specificity = recall_score(y_test, y_pred, pos_label=0)
+print("specificity: ", specificity)
+
+# Puntuación F1:
+from sklearn.metrics import f1_score
+f1 = f1_score(y_test, y_pred)
+print("f1 score: ", f1)
+
+# Área bajo la curva:
+from sklearn.metrics import roc_auc_score
+auc = roc_auc_score(y_test, y_pred)
+print("auc: ", auc)
+
+# Curva ROC
+from sklearn.metrics import roc_curve
+plt.figure()
+lw = 2
+plt.plot(roc_curve(y_test, y_pred)[0], roc_curve(y_test, y_pred)[1], color='darkorange',lw=lw, label='ROC curve (area = %0.2f)' %auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+# R Score (R^2 coefficient of determination)
+from sklearn.metrics import r2_score
+R = r2_score(y_test, y_pred)
+print("R2: ", R)
+
+# Guardar el modelo a un archivo
+import joblib
+joblib.dump(model, 'logistic_regression_model.pkl')
+# Cargar el modelo desde el archivo
+loaded_model = joblib.load('logistic_regression_model.pkl')
+# Hacer predicciones con el modelo cargado
+y_pred = model.predict(X_test)
